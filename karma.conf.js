bd33c7e3e0deaa98af1bd26346321d3b196bd5d1
Omar Benhamida <67659891+OmarBenhamida@users.noreply.github.com>
2024-01-16 11:11:26 +0100

    yfathi readMe

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sofapps-frontend'),
+      dir: require('path').join(__dirname, './coverage/pilpose-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  pilposeHost: "http://localhost:8888",
 };
